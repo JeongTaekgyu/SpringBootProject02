a88bc49c1d4418eedec89fb4fa2aca5f0e8a4194
taekgyu <taekgyu0602@gmail.com>
2022-01-30 00:21:03 +0900

    initial commit

---
+++ src/main/java/com/taek/w4springpjt/W4springPjtApplication.java
@@ -0,0 +1,13 @@
+package com.taek.w4springpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4springPjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W4springPjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taek/w4springpjt/W4springPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taek.w4springpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W4springPjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
